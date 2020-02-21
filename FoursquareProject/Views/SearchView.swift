f3bc62565f2a138165634048f873fe4db6f58320
maitreebain <maitreebain@gmail.com>
2020-02-21 17:13:44 -0500

    core location file added

--- FoursquareProject/Views/SearchView.swift
+++ FoursquareProject/Views/SearchView.swift
@@ -9,13 +9,17 @@
 import UIKit
 
 class SearchView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func commonInit() {
+        
+    }
+    
 }
