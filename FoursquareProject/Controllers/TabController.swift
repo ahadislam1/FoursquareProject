7d69fc9b1cab1e98bc943e61998851c526c77b65
Ahad Islam <ahad.islam624@gmail.com>
2020-02-21 14:50:29 -0500

    added in the necessary views and controllers

---
+++ FoursquareProject/Controllers/TabController.swift
@@ -0,0 +1,30 @@
+//
+//  TabController.swift
+//  FoursquareProject
+//
+//  Created by Ahad Islam on 2/21/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class TabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FoursquareProject/CreateView.swift
@@ -0,0 +1,21 @@
+//
+//  CreateView.swift
+//  FoursquareProject
+//
+//  Created by Ahad Islam on 2/21/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class CreateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
