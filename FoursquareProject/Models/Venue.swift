3cf05d162721ba8f7b3a20df87d91a672f5b94a0
Ahad Islam <ahad.islam624@gmail.com>
2020-02-21 14:54:38 -0500

    added in swift package manager, and a search view controller and a searcdhview

---
+++ FoursquareProject/Models/Venue.swift
@@ -0,0 +1,13 @@
+//
+//  Venue.swift
+//  FoursquareProject
+//
+//  Created by Ahad Islam on 2/21/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import Foundation
+
+struct Venue {
+    
+}
---
+++ FoursquareProject/Views/SearchView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchView.swift
+//  FoursquareProject
+//
+//  Created by Ahad Islam on 2/21/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class SearchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
