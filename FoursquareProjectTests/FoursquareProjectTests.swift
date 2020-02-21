ca370ccc27a48f21efde008dd3930f450d6116b8
Ahad Islam <ahad.islam624@gmail.com>
2020-02-21 16:18:10 -0500

    added unit test for the model

--- FoursquareProjectTests/FoursquareProjectTests.swift
+++ FoursquareProjectTests/FoursquareProjectTests.swift
@@ -7,9 +7,28 @@
 //
 
 import XCTest
+import DataPersistence
 @testable import FoursquareProject
 
 class FoursquareProjectTests: XCTestCase {
     
+    func testModel() {
+        var venues = [Venue]()
+        let endpointURL = "https://api.foursquare.com/v2/venues/search?client_id=\(Secret.clientID)&client_secret=\(Secret.clientSecret)&v=20200221&ll=40.735,-73.78"
+        let exp = XCTestExpectation(description: "Testing something.")
+        
+        GenericCoderAPI.manager.getJSON(objectType: VenueModel.self, with: endpointURL) { result in
+            switch result {
+            case .failure(let error):
+                XCTFail("\(error)")
+            case .success(let model):
+                venues = model.response.venues
+                XCTAssert(venues.isEmpty != true)
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 2)
+    }
 
 }
