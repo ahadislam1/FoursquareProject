b0881477b2ba5574ab379f912e0b1e1297a2cb0d
Tiffany Obi <tiffanyobi123@gmail.com>
2020-02-24 15:53:25 -0500

    small view additons

--- FoursquareProject/Controllers/DetailViewController.swift
+++ FoursquareProject/Controllers/DetailViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    private let detailView = DetailView()
+       
+       override func loadView() {
+          view = detailView
+       }
     override func viewDidLoad() {
         super.viewDidLoad()
 
