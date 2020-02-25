41ce0f11ad50fe24be563e185a5522dae93df310
Ahad Islam <ahad.islam624@gmail.com>
2020-02-25 18:00:46 -0500

    Created new model. added unit testing for specific model.

---
+++ FoursquareProject/Models/Photo.swift
@@ -0,0 +1,107 @@
+//
+//  Photo.swift
+//  FoursquareProject
+//
+//  Created by Ahad Islam on 2/25/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - PhotoWrapper
+struct PhotoWrapper: Codable {
+    let meta: Meta
+    let response: Response
+
+    enum CodingKeys: String, CodingKey {
+        case meta = "meta"
+        case response = "response"
+    }
+}
+
+// MARK: - Meta
+struct Meta: Codable {
+    let code: Int
+    let requestID: String
+
+    enum CodingKeys: String, CodingKey {
+        case code = "code"
+        case requestID = "requestId"
+    }
+}
+
+// MARK: - Response
+struct Response: Codable {
+    let photos: Photos
+
+    enum CodingKeys: String, CodingKey {
+        case photos = "photos"
+    }
+}
+
+// MARK: - Photos
+struct Photos: Codable {
+    let count: Int
+    let items: [Item]
+    let dupesRemoved: Int
+
+    enum CodingKeys: String, CodingKey {
+        case count = "count"
+        case items = "items"
+        case dupesRemoved = "dupesRemoved"
+    }
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let id: String
+    let createdAt: Int
+    let source: Source
+    let prefix: String
+    let suffix: String
+    let width: Int
+    let height: Int
+    let checkin: Checkin
+    let visibility: String
+    var imageURL: String { prefix + suffix }
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case createdAt = "createdAt"
+        case source = "source"
+        case prefix = "prefix"
+        case suffix = "suffix"
+        case width = "width"
+        case height = "height"
+        case checkin = "checkin"
+        case visibility = "visibility"
+    }
+}
+
+// MARK: - Checkin
+struct Checkin: Codable {
+    let id: String
+    let createdAt: Int
+    let type: String
+    let timeZoneOffset: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case createdAt = "createdAt"
+        case type = "type"
+        case timeZoneOffset = "timeZoneOffset"
+    }
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let name: String
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case url = "url"
+    }
+}
+
+
--- FoursquareProjectTests/FoursquareProjectTests.swift
+++ FoursquareProjectTests/FoursquareProjectTests.swift
@@ -14,7 +14,7 @@ class FoursquareProjectTests: XCTestCase {
     
     func testModel() {
         var venues = [Venue]()
-        let endpointURL = "https://api.foursquare.com/v2/venues/search?client_id=\(Secret.clientID)&client_secret=\(Secret.clientSecret)&v=20200221&ll=40.735,-73.78"
+        let endpointURL = "https://api.foursquare.com/v2/venues/search?client_id=\(Secret.appId)&client_secret=\(Secret.appSecret)&v=20200221&ll=40.735,-73.78"
         let exp = XCTestExpectation(description: "Testing something.")
         
         GenericCoderAPI.manager.getJSON(objectType: VenueModel.self, with: endpointURL) { result in
@@ -24,6 +24,25 @@ class FoursquareProjectTests: XCTestCase {
             case .success(let model):
                 venues = model.response.venues
                 XCTAssert(venues.isEmpty != true)
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 2)
+    }
+    
+    func testImages() {
+        let venueID = "48427799f964a5203c501fe3"
+        let endpointURL = "https://api.foursquare.com/v2/venues/\(venueID)/photos?client_id=\(Secret.appId)&client_secret=\(Secret.appSecret)&v=20200221"
+        var photos = [Item]()
+        let exp = XCTestExpectation(description: "Dont talk to me")
+        
+        GenericCoderAPI.manager.getJSON(objectType: PhotoWrapper.self, with: endpointURL) { result in
+            switch result {
+            case .failure(let error):
+                XCTFail("\(error)")
+            case .success(let wrapper):
+                photos = wrapper.response.photos.items
                 exp.fulfill()
             }
         }
